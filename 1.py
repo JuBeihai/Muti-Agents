153e79d7526b032d6c260ea8b1ed551247e620dd
Mei Jiaxuan <2827152361@qq.com>
2026-05-07 18:31:37 +0800

    2

--- 1.py
+++ 1.py
@@ -1,3 +1,15 @@
-from langchain_community.llms import togyi
+from langchain_community.llms import tongyi
 
 
+# sk-3379d8673c5b4c2d846e24f65afc35a8
+
+
+
+key= 'sk-3379d8673c5b4c2d846e24f65afc35a8'
+
+llm = tongyi.Tongyi(api_key = key)
+
+
+# 调用
+
+llm.invoke("夏天适合吃什么水果？")
